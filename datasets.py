7f8aeb1ff9df31fa3e13ee3565105412e0876842
Christian Haack <43646111+chrhck@users.noreply.github.com>
2021-02-24 17:19:15 +0100

    Create datasets.py

---
+++ datasets.py
@@ -0,0 +1,12 @@
+datasets = [    
+    ("Snowstorm_NuMu_LowE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21430/*/*.i3.zst"),
+    ("Snowstorm_NuMu_MidE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21431/*/*.i3.zst"),
+    ("Snowstorm_NuMu_HiE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21432/*/*.i3.zst"),
+    ("Snowstorm_NuE_LowE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21468/*/*.i3.zst"),
+    ("Snowstorm_NuE_MidE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21469/*/*.i3.zst"),
+    ("Snowstorm_NuE_HiE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21470/*/*.i3.zst"),
+    ("Snowstorm_NuTau_LowE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21471/*/*.i3.zst"),
+    ("Snowstorm_NuTau_MidE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21472/*/*.i3.zst"),
+    ("Snowstorm_NuTau_HiE", "/data/sim/IceCube/2016/filtered/level2/neutrino-generator/21473/*/*.i3.zst"),
+    ("CORSIKA_20904", "/data/sim/IceCube/2016/filtered/level2/CORSIKA-in-ice/20904/*/*.i3.zst"),
+]
